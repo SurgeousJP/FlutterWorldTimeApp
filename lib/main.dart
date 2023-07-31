9a134a00c754daa93b3c9bf62af45b1d504ae555
SurgeousJP <tuanbao2311@gmail.com>
2023-07-31 10:24:09 +0700

    Maps & Routing in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
+import 'pages/loading.dart';
+import 'pages/choose_location.dart';
+
 void main() {
-  runApp(const MaterialApp(
-    home: Home()
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => const Loading(),
+      '/home': (context) => const Home(),
+      '/choose_location': (context) => const ChooseLocation()
+    },
   ));
 }
 
