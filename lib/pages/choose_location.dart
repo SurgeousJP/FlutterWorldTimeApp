ad5c4f527ff9d5abfa7419736b5a13209155024f
SurgeousJP <tuanbao2311@gmail.com>
2023-07-31 10:43:17 +0700

    Asynchronous in Flutter (async & await)

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,11 +11,27 @@ class _ChooseLocationState extends State<ChooseLocation> {
 
   // Widget Life Cycle initState() -> build() (called when the setState() ran) -> Dispose()
 
+  void getData() async{
+
+    // simulate network request for a user
+    String username = await Future.delayed(const Duration(seconds: 3), () {
+      return 'yoshi';
+    });
+
+    // simulate network request for the bio given the username
+    String bio = await Future.delayed(const Duration(seconds: 2), () {
+      return 'vega, musician';
+    });
+
+    print('$username - $bio');
+  }
+
   int counter = 0;
 
   @override
   void initState() {
     super.initState();
+    getData();
     print('initState function ran');
   }
 
